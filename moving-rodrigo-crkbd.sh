cb71d33867edb998c213157873df85681c7ed1b9
Rogarpad <rodrigoestrategia2000@gmail.com>
2024-06-09 11:12:21 -0600

    [hotfix]flash: Add flashing script for moving out firmware to flash with qmk configurator

---
+++ moving-rodrigo-crkbd.sh
@@ -0,0 +1,3 @@
+qmk compile -kb crkbd -km rodrigo
+rm /media/sf_VM2025-1Shared/qmk_sofle_venta/alternativos/crkbd_rev1_rodrigo.hex
+mv /home/rodrigo/qmk_firmware/crkbd_rev1_rodrigo.hex /media/sf_VM2025-1Shared/qmk_sofle_venta/alternativos/
