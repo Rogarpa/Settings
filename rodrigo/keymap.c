6f9678382e914d11e6b4e75fb1ca37ad67f8ad4f
Rogarpad <rodrigoestrategia2000@gmail.com>
2024-06-09 11:06:13 -0600

    [feat] Add navigation SO desktops and tab correction

--- rodrigo/keymap.c
+++ rodrigo/keymap.c
@@ -29,6 +29,7 @@ enum custom_keycodes {
   LOWER,
   RAISE,
   ADJUST,
+  TOGGLE_ALTTAB,
   //RGBRST,
     //MACRO1,
   //MACRO2,
@@ -64,13 +65,13 @@ static td_state_t td_state;
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_QWERTY] = LAYOUT(
   //|-----------------------------------------------------|                    |-----------------------------------------------------|
-     KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                         KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
+     KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                         KC_Y,    KC_U,             KC_I,    KC_O,    KC_P,    KC_BSPC,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
      KC_TAB,  KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                          KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,  KC_QUOT,
   //---------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-     TD(TD_CAPLOCK), KC_Z,   KC_X,    KC_C,    KC_V,    KC_B,                 KC_N,    KC_M,    KC_COMM,   KC_DOT,  KC_SLSH, MT(MOD_LSFT, KC_ENT),
+     TD(TD_CAPLOCK), KC_Z,            KC_X,    KC_C,    KC_V,    KC_B,                 KC_N,    KC_M,    KC_COMM,   KC_DOT,  KC_SLSH, MT(MOD_LSFT, KC_ENT),
   //---------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                            TD(TD_LGUICTRL), LOWER, KC_ENT,              KC_SPC, RAISE, KC_LALT
+                            TD(TD_LGUICTRL), LOWER, MT(MOD_LALT,KC_SPC),              KC_SPC, RAISE, KC_LALT
                                       //|--------------------------|  |--------------------------|
 
 
@@ -80,9 +81,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //|-----------------------------------------------------|                    |-----------------------------------------------------|
      KC_DEL,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                         KC_6,    KC_7,    KC_8,    KC_9,    KC_0,   KC_BSPC,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-     LALT(KC_TAB), KC_LEFT, KC_DOWN, KC_UP, KC_RIGHT, KC_F6,                     KC_MINS, KC_EQL, KC_LBRC,  KC_RBRC, KC_PIPE, KC_BSLS,
+     TOGGLE_ALTTAB, KC_LEFT, KC_DOWN, KC_UP, KC_RIGHT, KC_F6,                     KC_MINS, KC_EQL, KC_LBRC,  KC_RBRC, KC_PIPE, KC_BSLS,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-     KC_LGUI, XXXXXXX, KC_F2, XXXXXXX, LALT(KC_F4), LALT(KC_D),                  RALT(KC_N), LGUI(LCTL(KC_RIGHT)) , XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+     KC_LGUI, XXXXXXX, KC_F2, XXXXXXX, LALT(KC_F4), LALT(KC_D),                  RALT(KC_N), LGUI(LCTL(KC_LEFT)) , RALT(LCTL(KC_DOWN)), RALT(LCTL(KC_UP)), LGUI(LCTL(KC_RIGHT)), XXXXXXX,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
                                          RCTL(KC_F), KC_TRNS, XXXXXXX,    XXXXXXX, RAISE, KC_RALT
                                       //|--------------------------|  |--------------------------|
@@ -578,14 +579,18 @@ bool oled_task_user(void) {
 
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    static bool is_tab_toggled;
   switch (keycode) {
     case LOWER:
       if (record->event.pressed) {
         layer_on(_LOWER);
-        update_tri_layer_RGB(_LOWER, _RAISE, _ADJUST);
+        // update_tri_layer_RGB(_LOWER, _RAISE, _ADJUST);
       } else {
         layer_off(_LOWER);
-        update_tri_layer_RGB(_LOWER, _RAISE, _ADJUST);
+        // update_tri_layer_RGB(_LOWER, _RAISE, _ADJUST);
+        unregister_code(KC_LALT);
+        is_tab_toggled ^= 1;
+
       }
       return false;
     case RAISE:
@@ -604,9 +609,22 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
           layer_off(_ADJUST);
         }
         return false;
+    case TOGGLE_ALTTAB:
+            if (record->event.pressed) {
+                is_tab_toggled ^= 1;
+                if (is_tab_toggled) { 
+                    register_code(KC_LALT);
+                    register_code(KC_TAB);
+                    unregister_code(KC_TAB);                                                                                                                                                  
+                } else { 
+                    register_code(KC_TAB);
+                    unregister_code(KC_TAB);
+                }
+            }
+        return false;
         
 //        case MACRO1:
-//    if (record->event.pressed) {
+//    if (record->event.pressed) {  
 //            SEND_STRING("Aqui pones lo que quieras en el macro 1");
 //        } else {
 //            
