7287407b26b38b4b1b96eda0e51a3a33c85fff70
Rogarpad <rodrigoestrategia2000@gmail.com>
2024-09-28 06:21:34 -0600

    Mod compilation filepath

--- moving-rodrigo-crkbd.sh
+++ moving-rodrigo-crkbd.sh
@@ -1,3 +1,3 @@
 qmk compile -kb crkbd -km rodrigo
 rm /media/sf_VM2025-1Shared/qmk_sofle_venta/cornetester/crkbd_rev1_rodrigo.hex
-mv /home/rodrigo/qmk_firmware/crkbd_rev1_rodrigo.hex /media/sf_VM2025-1Shared/qmk_sofle_venta/cornetester/dani-help-enablespace
+mv /home/rodrigo/qmk_firmware/crkbd_rev1_rodrigo.hex /media/sf_VM2025-1Shared/qmk_sofle_venta/cornetester/dani-enablespace.hex
