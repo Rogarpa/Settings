e15c58eaeb83c7043ff2e9cb5a1781c52a2f2f55
Rogarpad <rodrigoestrategia2000@gmail.com>
2024-10-05 14:46:53 -0600

    [feature]compilation: Add compilation script

---
+++ viargb/moving-rodrigo-crkbd.sh
@@ -0,0 +1,4 @@
+qmk clean
+qmk compile -kb sofle -km viargb
+rm /media/sf_VM2025-1Shared/qmk_sofle_venta/cornetester/sofle_rev1_viargb.hex
+mv /home/rodrigo/qmk_firmware/sofle_rev1_viargb.hex /media/sf_VM2025-1Shared/qmk_sofle_venta/cornetester/
